e6ed26f8e7eae90702f649632dcb29f9cd672bab
sheimy <sheimypazserpa@gmail.com>
2023-02-04 22:27:34 -0500

    functions update to get user inputs

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from wtforms import FileField, SubmitField, StringField, IntegerField
 from werkzeug.utils import secure_filename
 import os
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Optional
+from astroplan import Observer,FixedTarget
+from astropy.coordinates import SkyCoord
+from astroquery.mast import Tesscut
+from astropy import units as u
 
 
 app = Flask(__name__)
@@ -14,18 +18,67 @@ class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
+class QueryForm(FlaskForm):
+    query = StringField("Query", validators=[InputRequired()])
+    sector = IntegerField("Sector", validators=[Optional()])
+    submit = SubmitField("Submit Query")
 
-# Root URL
-@app.route('/', methods=["GET","POST"])
-@app.route('/home', methods=["GET","POST"])
-def home():
-    form = UploadFileForm()
-    if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return "File has been uploaded."
-    return render_template('index.html', form=form)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/sectors", methods=["POST"])
+def sectors():
+    search_input = request.form.get("search_input")
+    radius = request.form.get("radius")
+
+    coord = None
+    ra = None
+    dec = None
+    object_name = None
+    tic_id = None
+
+    try:
+        coord = SkyCoord(search_input, unit = "deg")
+        ra = coord.ra.degree
+        dec = coord.dec.degree
+    except:
+        if " " in search_input:
+            try:
+                ra, dec = search_input.split(" ")
+                coord = SkyCoord(f"{ra} {dec}", unit = "deg")
+            except:
+                pass
+        if not coord:
+            if search_input.isdigit():
+                tic_id = search_input
+                object_name = "TIC " + tic_id
+            else:
+                object_name = search_input
+
+    if coord:
+        sectors = Tesscut.get_sectors(coordinates=coord, radius=float(radius)*u.deg)
+    elif object_name:
+        sectors = Tesscut.get_sectors(objectname=object_name, radius=float(radius)*u.deg)
+    elif tic_id:
+        sectors = Tesscut.get_sectors(objectname=tic_id, radius=float(radius)*u.deg)
+    else:
+        return "Error: Please provide either RA and Dec or Object Name or TIC ID."
+
+    results = []
+    for sector in sectors:
+        sector_number = sector['sector']
+        cycle = (sector_number - 1) // 13 + 1
+        camera = sector['camera']
+        result = [sector_number, cycle, camera]
+        results.append(result)
+
+    return render_template("index.html", results=results)
+
+
 
 
 if __name__ == "__main__":
-     app.run(debug=True)
+    app.run(debug=True)  
+
+
