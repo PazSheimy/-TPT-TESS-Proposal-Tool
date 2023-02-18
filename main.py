50d8e2f9efd681563f687d482525fee2eb99df28
sheimy <sheimypazserpa@gmail.com>
2023-02-17 21:43:44 -0500

    adding hr diagram

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import os
 from wtforms.validators import InputRequired, Optional
 from astroplan import Observer,FixedTarget
 from astropy.coordinates import SkyCoord
-from astroquery.mast import Tesscut
+from astroquery.mast import Tesscut, Catalogs
 from astropy import units as u
-
+from bokeh.plotting import figure
+from bokeh.embed import file_html
+from bokeh.resources import CDN
+from bokeh.resources import Resources
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -28,34 +31,133 @@ def index():
     return render_template("index.html")
 
 @app.route("/sectors", methods=["POST"])
-def sectors():
+# def sectors():
+#     search_input = request.form.get("search_input")
+#     radius = request.form.get("radius")
+
+#     coord = None
+#     ra = None
+#     dec = None
+#     object_name = None
+#     tic_id = None
+
+#     try:
+#         coord = SkyCoord(search_input, unit = "deg")
+#         ra = coord.ra.degree
+#         dec = coord.dec.degree
+#     except:
+#         if " " in search_input:
+#             try:
+#                 ra, dec = search_input.split(" ")
+#                 coord = SkyCoord(f"{ra} {dec}", unit = "deg")
+#             except:
+#                 pass
+#         if not coord:
+#             if search_input.isdigit():
+#                 tic_id = search_input
+#                 object_name = "TIC " + tic_id
+#             else:
+#                 object_name = search_input
+
+#     if coord:
+#         sectors = Tesscut.get_sectors(coordinates=coord, radius=float(radius)*u.deg)
+#     elif object_name:
+#         sectors = Tesscut.get_sectors(objectname=object_name, radius=float(radius)*u.deg)
+#     elif tic_id:
+#         sectors = Tesscut.get_sectors(objectname=tic_id, radius=float(radius)*u.deg)
+#     else:
+#         return "Error: Please provide either RA and Dec or Object Name or TIC ID."
+
+#     results = []
+#     for sector in sectors:
+#         sector_number = sector['sector']
+#         cycle = (sector_number - 1) // 13 + 1
+#         camera = sector['camera']
+#         result = [sector_number, cycle, camera]
+#         results.append(result)
+
+#      # Get the HR diagram information for the target object
+#     if coord:
+#         tess_cutout = Tesscut.download_cutouts(coordinates=coord, size=5)
+#         metadata = Catalogs.query_region(coord, radius=30 * u.arcmin,catalog="Tic")
+#         star_name = f"{ra} {dec}"
+#     elif object_name:
+#         tess_cutout = Tesscut.download_cutouts(objectname=object_name, size=5)
+#         metadata = Catalogs.query_object(object_name, catalog="Tic")
+#         star_name = object_name
+#     else:
+#         return "Error: Please provide either RA and Dec or Object Name or TIC ID."
+
+
+#     # Extracting the values of the "lum" and "Teff" columns
+#     luminosity = metadata['lum'] * u.solLum
+#     temperature = metadata['Teff'] * u.K
+    
+#     # Create the HR diagram plot
+#     p = figure(title=f"HR Diagram for {star_name} (Sector {sector_number})")
+#     p.circle(temperature, luminosity.to(u.W))
+#     p.xaxis.axis_label = "Temperature (K)"
+#     p.yaxis.axis_label = "Luminosity (solLum)" 
+
+#     resources = Resources(mode='cdn')
+#     html = file_html(p, resources=resources, title=f"HR Diagram for {star_name} (Sector {sector_number})")
+#     return render_template("index.html", results=results, star_name=object_name, sector_num=sector_number, diagram=html)
+
+
+
+
+
+
+
+
+def get_input():
+    # get the values of "search_input" and "radius" from the form data
     search_input = request.form.get("search_input")
     radius = request.form.get("radius")
 
+    # call the "sectors" function and store the returned values in variables
+    results, ra, dec, object_name, tic_id, coord, sector_number = sectors(search_input, radius)
+
+    # call the "hr_diagram" function and store the returned value in "html"
+    html = hr_diagram(coord, sector_number, object_name, tic_id, ra, dec)
+    
+    # Render the HTML in a template and return it
+    return render_template("index.html", results=results, star_name=object_name, sector_num=sector_number, diagram=html)
+
+def sectors(search_input, radius):
+    # initialize variables with None
     coord = None
     ra = None
     dec = None
     object_name = None
     tic_id = None
 
+    # try to convert the search_input into a SkyCoord object
     try:
         coord = SkyCoord(search_input, unit = "deg")
         ra = coord.ra.degree
         dec = coord.dec.degree
     except:
+        # if the conversion fails, check if the search_input contains a space
         if " " in search_input:
             try:
+                # if the search_input contains a space, split it into ra and dec
                 ra, dec = search_input.split(" ")
                 coord = SkyCoord(f"{ra} {dec}", unit = "deg")
             except:
+                # if this conversion also fails, coord will still be None
                 pass
+        # if coord is still None, check if the search_input is a digit
         if not coord:
             if search_input.isdigit():
+                # if the search_input is a digit, store it in tic_id
                 tic_id = search_input
                 object_name = "TIC " + tic_id
             else:
+                # if the search_input is not a digit, store it in object_name
                 object_name = search_input
 
+    # Query the sectors based on the type of the input (coord, object_name, or tic_id)
     if coord:
         sectors = Tesscut.get_sectors(coordinates=coord, radius=float(radius)*u.deg)
     elif object_name:
@@ -65,20 +167,69 @@ def sectors():
     else:
         return "Error: Please provide either RA and Dec or Object Name or TIC ID."
 
+    # create a list of results and store sector number, cycle, and camera for each sector
     results = []
     for sector in sectors:
-        sector_number = sector['sector']
-        cycle = (sector_number - 1) // 13 + 1
-        camera = sector['camera']
-        result = [sector_number, cycle, camera]
-        results.append(result)
+        sector_number = sector['sector'] # Retrieve the sector number
+        cycle = (sector_number - 1) // 13 + 1 # Calculate the cycle number
+        camera = sector['camera'] # Retrieve the camera information
+        result = [sector_number, cycle, camera] # Combine the information into a list
+        results.append(result) # Add the list to the results
+    
+    
+    return results, ra, dec, object_name, tic_id, coord, sector_number
+    
 
-    return render_template("index.html", results=results)
+def hr_diagram(coord, sector_number, object_name, tic_id, ra, dec):
+    # Check if the input is a coordinate
+    if coord:
+        # Download the TESS cutout using the coordinate
+        tess_cutout = Tesscut.download_cutouts(coordinates=coord, size=5)
+        # Query the metadata of the star using the coordinate
+        metadata = Catalogs.query_region(coord, radius=30 * u.arcmin,catalog="Tic")
+        star_name = f"{coord.ra.degree} {coord.dec.degree}"
+    # Check if the input is an object name
+    elif object_name:
+        # Download the TESS cutout using the object name
+        tess_cutout = Tesscut.download_cutouts(objectname=object_name, size=5)
+        # Query the metadata of the star using the object name
+        metadata = Catalogs.query_object(object_name, catalog="Tic")
+        star_name = object_name
+    # Check if the input is a TIC ID
+    elif tic_id:
+        # Download the TESS cutout using the TIC ID
+        tess_cutout = Tesscut.download_cutouts(objectname=tic_id, size=5)
+        # Query the metadata of the star using the TIC ID
+        metadata = Catalogs.query_object(tic_id, catalog="Tic")
+        star_name = "TIC " + tic_id
+    else:
+        # Return None if no input is provided
+        return None
+    # Retrieve the luminosity and temperature from the metadata
+    luminosity = metadata['lum'] * u.solLum
+    temperature = metadata['Teff'] * u.K
 
+    # Create a figure with the title HR Diagram for [star_name] (Sector [sector_number])
+    p = figure(title=f"HR Diagram for {star_name} (Sector {sector_number})")
+    # Add a scatter plot of temperature vs. luminosity to the figure
+    p.circle(temperature, luminosity.to(u.W))
+    # Label the x-axis as Temperature (K)
+    p.xaxis.axis_label = "Temperature (K)"
+    # Label the y-axis as Lumin
+    p.yaxis.axis_label = "Luminosity (solLum)"
+
+    #Set mode of resources to cdn for faster loading
+    resources = Resources(mode='cdn')
+    #Create HTML file from figure with specified title
+    html = file_html(p, resources=resources, title=f"HR Diagram for {star_name} (Sector {sector_number})")
+
+    return html 
+    
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)  
+
 
 
