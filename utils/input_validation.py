a3e5d7220161af199c9b26e5449f9acdad088f92
PMccormick1 <pmccormick2022@fau.edu>
2023-04-13 14:17:20 -0400

    added new pages. updated media queries

--- utils/input_validation.py
+++ utils/input_validation.py
@@ -45,12 +45,12 @@ def get_input():
     is_valid, validation_result = validate_inputs(search_input, radius, sector_selection)
 
     if not is_valid:
-        return render_template("index.html", error=validation_result)
+        return render_template("target_visualization.html", error=validation_result)
     
     search_input, radius, sector_number = validation_result
 
     try:
         processed_data = process_data(search_input, radius, sector_number)
-        return render_template("index.html", **processed_data)
+        return render_template("target_visualization.html", **processed_data)
     except requests.exceptions.ConnectTimeout:
-        return render_template("index.html", error="The connection to the MAST API timed out. Please try again later.")
+        return render_template("target_visualization.html", error="The connection to the MAST API timed out. Please try again later.")
